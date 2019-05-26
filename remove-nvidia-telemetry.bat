f3bc1472f5659e240bc8e6ab15ccadb3917d7b1e
mcmetrox <mcmetrox+github@gmail.com>
2019-05-26 21:06:01 +0200

    add checks if files/dirs are present before modifing/deleting

--- remove-nvidia-telemetry.bat
+++ remove-nvidia-telemetry.bat
@@ -14,60 +14,80 @@ for %%p in (%xmlstarlet_exe%) do (
 )
 
 set dir=NvTelemetry
-echo [33m[removing dir][0m %dir%
-RMDIR /S /Q "%cd%\%dir%" >NUL  2>NUL
+if exist "%cwd%\%dir%\" (
+    echo [33m[removing dir][0m %dir%
+    RMDIR /S /Q "%cwd%\%dir%" >NUL  2>NUL
+)
 
 set file=NvContainer\NvContainerTelemetryApi.nvi
-echo [33m[removing file][0m %file%
-DEL /Q /S "%cd%\%file%" >NUL  2>NUL
+if exist "%cwd%\%file%" (
+    echo [33m[removing file][0m %file%
+    DEL /Q /S "%cwd%\%file%" >NUL  2>NUL
+)
 
 set file=NvContainer\x86\NvContainerTelemetryApi.dll
-echo [33m[removing file][0m %file%
-DEL /Q /S "%cd%\%file%" >NUL  2>NUL
+if exist "%cwd%\%file%" (
+    echo [33m[removing file][0m %file%
+    DEL /Q /S "%cwd%\%file%" >NUL  2>NUL
+)
 
 set file=NvContainer\x86_64\NvContainerTelemetryApi.dll
-echo [33m[removing file][0m %file%
-DEL /Q /S "%cd%\%file%" >NUL  2>NUL
+if exist "%cwd%\%file%" (
+    echo [33m[removing file][0m %file%
+    DEL /Q /S "%cwd%\%file%" >NUL  2>NUL
+)
 
 set file=Update.Core\UpdateCore.nvi
-echo [33m[updating file][0m %file%
-%xmlstarlet_exe% ed --inplace --delete "/nvi/manifest/file[starts-with(@name,'NvTm') and contains(@name,'.exe')]" "%cd%\%file%"
-
-%xmlstarlet_exe% ed --inplace --delete "/nvi/phases/standard[@phase='copyx86BackendBinaries']/copyFile[starts-with(@target,'NvTm') and contains(@target,'.exe')]" "%cd%\%file%"
-
-%xmlstarlet_exe% ed --inplace --delete "/nvi/phases/standard[starts-with(@phase,'scheduleNvTm') and scheduleTask[@action='create']]" "%cd%\%file%"
+if exist "%cwd%\%file%" (
+    echo [33m[updating file][0m %file%
+    %xmlstarlet_exe% ed --inplace --delete "/nvi/manifest/file[starts-with(@name,'NvTm') and contains(@name,'.exe')]" "%cwd%\%file%"
+    %xmlstarlet_exe% ed --inplace --delete "/nvi/phases/standard[@phase='copyx86BackendBinaries']/copyFile[starts-with(@target,'NvTm') and contains(@target,'.exe')]" "%cwd%\%file%"
+    %xmlstarlet_exe% ed --inplace --delete "/nvi/phases/standard[starts-with(@phase,'scheduleNvTm') and scheduleTask[@action='create']]" "%cwd%\%file%"
+)
 
 set file=Display.Driver\DisplayDriver.nvi
-echo [33m[updating file][0m %file%
-%xmlstarlet_exe% ed --inplace --update "/nvi/properties/bool[@name='UsesNvTelemetry']/@value" --value "false" "%cd%\%file%"
-
-%xmlstarlet_exe% ed --inplace --update "//exe[contains(@condition,'Global:EnableTelemetry')]/arg[contains(@value,'-enableTelemetry:true')]" --value "-enableTelemetry:false" "%cd%\%file%"
+if exist "%cwd%\%file%" (
+    echo [33m[updating file][0m %file%
+    %xmlstarlet_exe% ed --inplace --update "/nvi/properties/bool[@name='UsesNvTelemetry']/@value" --value "false" "%cwd%\%file%"
+    %xmlstarlet_exe% ed --inplace --update "//exe[contains(@condition,'Global:EnableTelemetry')]/arg[contains(@value,'-enableTelemetry:true')]" --value "-enableTelemetry:false" "%cwd%\%file%"
+)
 
 set file=DisplayDriverCrashAnalyzer\DisplayDriverCrashAnalyzer.nvi
-echo [33m[updating file][0m %file%
-%xmlstarlet_exe% ed --inplace --update "/nvi/properties/bool[@name='UsesNvTelemetry']/@value" --value "false" "%cd%\%file%"
+if exist "%cwd%\%file%" (
+    echo [33m[updating file][0m %file%
+    %xmlstarlet_exe% ed --inplace --update "/nvi/properties/bool[@name='UsesNvTelemetry']/@value" --value "false" "%cwd%\%file%"
+)
 
 set file=GFExperience.NvStreamSrv\GFExperience.NvStreamSrv.nvi
-echo [33m[updating file][0m %file%
-%xmlstarlet_exe% ed --inplace --update "/nvi/properties/bool[@name='UsesNvTelemetry']/@value" --value "false" "%cd%\%file%"
+if exist "%cwd%\%file%" (
+    echo [33m[updating file][0m %file%
+    %xmlstarlet_exe% ed --inplace --update "/nvi/properties/bool[@name='UsesNvTelemetry']/@value" --value "false" "%cwd%\%file%"
+)
 
 set file=nodejs\nodejs.nvi
-echo [33m[updating file][0m %file%
-%xmlstarlet_exe% ed --inplace --update "/nvi/properties/bool[@name='UsesNvTelemetry']/@value" --value "false" "%cd%\%file%"
+if exist "%cwd%\%file%" (
+    echo [33m[updating file][0m %file%
+    %xmlstarlet_exe% ed --inplace --update "/nvi/properties/bool[@name='UsesNvTelemetry']/@value" --value "false" "%cwd%\%file%"
+)
 
 set file=NvBackend\NvBackend.nvi
-echo [33m[updating file][0m %file%
-%xmlstarlet_exe% ed --inplace --update "/nvi/properties/bool[@name='UsesNvTelemetry']/@value" --value "false" "%cd%\%file%"
-
-%xmlstarlet_exe% ed --inplace --delete "/nvi/dependencies/package[@package='NvTelemetry']" "%cd%\%file%"
+if exist "%cwd%\%file%" (
+    echo [33m[updating file][0m %file%
+    %xmlstarlet_exe% ed --inplace --update "/nvi/properties/bool[@name='UsesNvTelemetry']/@value" --value "false" "%cwd%\%file%"
+    %xmlstarlet_exe% ed --inplace --delete "/nvi/dependencies/package[@package='NvTelemetry']" "%cwd%\%file%"
+)
 
 set file=NvCamera\NvCamera.nvi
-echo [33m[updating file][0m %file%
-%xmlstarlet_exe% ed --inplace --update "/nvi/properties/bool[@name='UsesNvTelemetry']/@value" --value "false" "%cd%\%file%"
+if exist "%cwd%\%file%" (
+    echo [33m[updating file][0m %file%
+    %xmlstarlet_exe% ed --inplace --update "/nvi/properties/bool[@name='UsesNvTelemetry']/@value" --value "false" "%cwd%\%file%"
+)
 
 set file=NvAbHub\NvAbHub.nvi
-echo [33m[updating file][0m %file%
-%xmlstarlet_exe% ed --inplace --delete "/nvi/dependencies/package[@package='NvTelemetry']" "%cd%\%file%"
+if exist "%cwd%\%file%" (
+    echo [33m[updating file][0m %file%
+    %xmlstarlet_exe% ed --inplace --delete "/nvi/dependencies/package[@package='NvTelemetry']" "%cwd%\%file%"
+)
 
 echo.
 echo [32m[DONE][0m
